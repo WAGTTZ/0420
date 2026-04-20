216c6a7f779202f8031ccf7ca70a12546458bd08
WAGTTZ <164768132+WAGTTZ@users.noreply.github.com>
2026-04-20 17:37:58 +0800

    Update mian.cpp

--- mian.cpp
+++ mian.cpp
@@ -1,1 +1,18 @@
 
+#include <iostream>
+using namespace std;
+
+// A 同學負責這部分
+void foo() {
+    
+}
+
+// B 同學負責這部分
+void bar() {
+    
+}
+
+int main() {
+    cout << "Hello, World!" << endl;
+    return 0;
+}
